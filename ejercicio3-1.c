160ab5b61a85d713e57c5f83b7b679540f35af88
ismaelisa <isaismael04@gmail.com>
2026-04-24 20:26:28 -0300

    ejercicio 3 terminado

--- ejercicio3-1.c
+++ ejercicio3-1.c
@@ -15,6 +15,7 @@ Nodo * CrearListaVacia();
 Nodo * CrearTarea(int id, char descripcion[50]);
 void InsertarNodo(Nodo ** Start , Nodo *Nodos);
 Nodo * QuitarNodo(Nodo **Start, int id);
+void Mostrar(Nodo * Start);
 int main(){
     srand(time(NULL));
     Nodo *Start;
@@ -23,7 +24,7 @@ int main(){
     int ID = 1000, id;
     char descripc[50];
     Start = CrearListaVacia();
-    Start = CrearListaVacia();
+    Start1 = CrearListaVacia();
     while(bandera == 1){
         fflush(stdin);
         printf("Agregue la descripcion de la tarea: \n");
@@ -35,16 +36,30 @@ int main(){
         printf("Quiere seguir agregando tareas(1) o finalizar la carga(0)? \n");
         scanf("%d", &bandera);
     }
+    printf("----Lista de tareas pendientes: -------\n\n");
+    Mostrar(Start);
     bandera = 1;
     while(bandera==1){
-        printf("Introduzca el Id de la traea que quiere marcar como realizada: \n");
-        scanf("%d",id);
-        Nodo *pendiente = QuitarNodo(&Start, id); //retorno del nodo que esta libre, 
-        InsertarNodo(&Start1, pendiente);
+        printf("Introduzca el Id de la tarea que quiere marcar como realizada: \n");
+        scanf("%d",&id);
+        Nodo *pendiente = QuitarNodo(&Start, id); //retorno del nodo que esta libre, que es apuntado por el puntero que devuelve quitarnodo y este nodo apunta al nulo
+        InsertarNodo(&Start1, pendiente); //inserto el nodo libre
         printf("Quiere seguir agregando tareas(1) o finalizar la carga(0)? \n");
         scanf("%d", &bandera);
     }
+    printf("----Lista de tareas realizadas: -------\n\n");
+    Mostrar(Start1);
     return 0;
+}
+void Mostrar(Nodo * Start){
+    Nodo *aux = Start;
+    while(aux != NULL){
+        printf("El Id de la tarea es: %d \n",aux->T.TareaID);
+        printf("La descripcion de esta tarea es: %s \n",aux->T.Descripcion);
+        printf("La duracion de esta tarea es: %d \n",aux->T.Duracion);
+        print("\n\n");
+        aux = aux->Siguiente;
+    }
 }
 Nodo * CrearListaVacia()
 {
@@ -69,25 +84,26 @@ Nodo * buscarNodo(Nodo * Start, int IdBuscado){
     Nodo * Aux = Start;
     while(Aux && Aux->T.TareaID != IdBuscado)
     {
-    Aux = Aux->Siguiente;
+        Aux = Aux->Siguiente;
     }
     return Aux;
 }
 Nodo * QuitarNodo(Nodo **Start, int id){
-    Nodo *nodoAux = (*Start);
-    Nodo *nodoAnt = NULL;
-    while (nodoAux != NULL && nodoAux->T.TareaID != id){
-        nodoAnt = nodoAux;
-        nodoAux = nodoAux->Siguiente;
+    Nodo *nodoAux = (*Start); //(*Start): Porque (*Start) contiene la dirección de memoria del primerísimo nodo de la lista. 
+    //Al igualarlos, estás colocando a tu explorador en la casilla de salida, listo para empezar a caminar
+    Nodo *nodoAnt = NULL; //el nodo anterior apunta al nulo
+    while (nodoAux != NULL && nodoAux->T.TareaID != id){ //mientras el nodoaux no llegue al final de la lista y no se encuentre el dato
+        nodoAnt = nodoAux; // el ant apunta al primer nodo ahora
+        nodoAux = nodoAux->Siguiente; // el aux ahora pasa a apuntar al siguiente nodo, osea al segundo o al nulo
     }
     if (nodoAux != NULL){
-        if (nodoAux == (*Start)){
-            (*Start) = nodoAux->Siguiente;
+        if (nodoAux == (*Start)){ // si el nodo buscado es el primer nodo
+            (*Start) = nodoAux->Siguiente; //hacemos que el puntero principal apunte al siguiente nod y dej libre al primero
         }
         else{
-            nodoAnt->Siguiente = nodoAux->Siguiente;
+            nodoAnt->Siguiente = nodoAux->Siguiente; // si esta en el medio o al ultimo hacemos que el anterior apunte al siguiente
         }
-        nodoAux->Siguiente = NULL;
+        nodoAux->Siguiente = NULL; //hacemos que el nodo apunte al nulo para evitar traer lo que seguia despues de el
     }
     return (nodoAux);
 }
